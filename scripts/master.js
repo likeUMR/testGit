e943e63f65f920c3a0f5096efb333a18a45126fa
likeUMR <2353146641@qq.com>
2021-08-15 10:12:00 +0800

    daily

---
+++ scripts/master.js
@@ -0,0 +1,8 @@
+var sega=document.querySelector('#sega');
+sega.onclick=function(){
+    alert("???");
+}
+var div=document.querySelector('#dd');
+div.onclick=function(){
+    this.style.visible='hidden';
+}
